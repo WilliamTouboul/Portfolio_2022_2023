d1284da4f7c7e560b8e034717aab79b5e4540435
William Touboul <Touboul.william@gmail.com>
2022-09-27 18:59:56 +0200

    modal

--- assets/script/main.js
+++ assets/script/main.js
@@ -49,34 +49,87 @@ gsap.to(sections, {
 /* -------------------------------------------------------------------------- */
 /*                              Drag & Drop Menu                              */
 /* -------------------------------------------------------------------------- */
-let x = 0;
-let y = 0;
+// let x = 0;
+// let y = 0;
 
-const ele = document.getElementById('dragMe');
+// const ele = document.getElementById('dragMe');
 
-const mouseDownHandler = function (e) {
-    x = e.clientX;
-    y = e.clientY;
-    document.addEventListener('mousemove', mouseMoveHandler);
-    document.addEventListener('mouseup', mouseUpHandler);
+// const mouseDownHandler = function (e) {
+//     x = e.clientX;
+//     y = e.clientY;
+//     document.addEventListener('mousemove', mouseMoveHandler);
+//     document.addEventListener('mouseup', mouseUpHandler);
+//     console.log(ele.getBoundingClientRect());
+// };
 
-};
+// const mouseMoveHandler = function (e) {
+//     const dx = e.clientX - x;
+//     const dy = e.clientY - y;
+//     ele.style.top = `${ele.offsetTop + dy}px`;
+//     ele.style.left = `${ele.offsetLeft + dx}px`;
+//     x = e.clientX;
+//     y = e.clientY;
+// };
 
-const mouseMoveHandler = function (e) {
-    const dx = e.clientX - x;
-    const dy = e.clientY - y;
-    ele.style.top = `${ele.offsetTop + dy}px`;
-    ele.style.left = `${ele.offsetLeft + dx}px`;
-    x = e.clientX;
-    y = e.clientY;
-};
+// const mouseUpHandler = function () {
+//     document.removeEventListener('mousemove', mouseMoveHandler);
+//     document.removeEventListener('mouseup', mouseUpHandler);
+// };
 
-const mouseUpHandler = function () {
-    document.removeEventListener('mousemove', mouseMoveHandler);
-    document.removeEventListener('mouseup', mouseUpHandler);
-};
+// ele.addEventListener('mousedown', mouseDownHandler);
 
-ele.addEventListener('mousedown', mouseDownHandler);
+
+
+members = document.querySelectorAll('.mover').forEach(item => {
+    item.addEventListener('mousedown', ev => {
+        console.log(ev);
+        let tid;
+        tid = ev.target.id;
+        if (ev.target.id == "") {
+            tid = ev.target.parentNode.id;
+        }
+        dragElement(document.getElementById(tid));
+    })
+})
+
+function dragElement(elmnt) {
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+    if (document.getElementById(elmnt.id + 'header')) {
+        document.getElementById(elmnt.id + 'header').onmousedown =
+            dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = elmnt.offsetTop - pos2 + 'px';
+        elmnt.style.left = elmnt.offsetLeft - pos1 + 'px';
+    }
+
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
 
 /* ------------------------------ Open & close ------------------------------ */
 let menu_open = true;
@@ -129,6 +182,13 @@ picker_1.addEventListener('click', function () {
     document.documentElement.style.setProperty('--clr__1', '#E0F8D0');
     document.documentElement.style.setProperty('--clr__2', '#88c070');
     document.documentElement.style.setProperty('--clr__3', ' #081820');
+
+    recolor_svg_stroke.forEach((item, index) => {
+        item.style.stroke = '#000000'
+    });
+    recolor_svg.forEach((item, index) => {
+        item.style.fill = '#E46E00'
+    })
 })
 
 
@@ -155,9 +215,37 @@ picker_3.addEventListener('click', function () {
     recolor_svg_stroke.forEach((item, index) => {
         item.style.stroke = '#FFFFFF'
     });
+
     recolor_svg.forEach((item, index) => {
         item.style.fill = '#F94755'
     })
 
 
 })
+
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                 Transition                                 */
+/* -------------------------------------------------------------------------- */
+// let all_links = document.querySelectorAll('a');
+// all_links.forEach((item, index) => {
+//     item.classList.add('link_transition')
+// });
+
+
+let selected_links = document.querySelectorAll('.link_transition');
+selected_links.forEach((item, index) => {
+    item.addEventListener('click', function (e) {
+        e.preventDefault();
+        let target_page = item.getAttribute('href');
+        gsap.to('.transition', {
+            top: 0,
+            duration: .5
+        })
+        setTimeout(function () {
+            window.location.href = target_page;
+        }, 1000);
+    })
+});
